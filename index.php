6508f8cc16606841728c6c2bb86cfcd9bdf6dd3f
Fabio-F16 <fipsfabio95@gmail.com>
2022-05-16 19:05:13 +0200

    add index

--- index.php
+++ index.php
@@ -9,10 +9,27 @@ include 'entities/includes.php';
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <title>Document</title>
 </head>
 <body>
-    
+    <div class="container">
+        <div class="row">
+            <div class="col-6">
+                <p> Nome Utente: <?php echo( $user1->name) ?></p>
+                <p> Cognome Utente: <?php echo( $user1->surname) ?></p>
+                <p> Status: <?php echo( $user1->setSconto($user1->registration)) ?></p>
+            </div>
+            <div class="col-6">
+                <p> Nome Utente: <?php echo( $user2->name) ?></p>
+                <p> Cognome Utente: <?php echo( $user2->surname) ?></p>
+                <p> Status: <?php echo( $user2->setSconto($user2->registration)) ?></p>
+            </div>
+        </div>
+    </div>
+
+
 <?php 
 
 ?>
