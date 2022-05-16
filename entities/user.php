f4c19574ac294d2f87a63cc701b1dc962755980d
Fabio-F16 <fipsfabio95@gmail.com>
2022-05-16 19:05:38 +0200

    add user

--- entities/user.php
+++ entities/user.php
@@ -6,28 +6,34 @@ class User{
     public $name;
     public $surname;
     private $userID;
+    public $registration = false;
     private $email;
     private $passwords;
     protected $sconto = 0;
-    public $registration = false;
+    
     // protected $creditCard;
 
 
-    function __construct($_name, $_surname, $_userID){
+    function __construct($_name, $_surname, $_userID, $_registration){
         $this->name = $_name;
         $this->surname = $_surname;
         $this->userID = rand(1,100);
+        $this->registration = $_registration;
     }
     
     public function setSconto($registration) {
         if($registration == false) {
             $this->sconto = 0;
+            echo('utente non registrato');
         } else {
             $this->sconto = 20;
+            echo('utente registrato');
         }
     }
-    
-  
 }
+$user1 = new User('Antonio', 'Falcone', 'xxx', true);
+$user2 = new User('Paola', 'Perego', '16280', false);
+
+
 
 ?>
